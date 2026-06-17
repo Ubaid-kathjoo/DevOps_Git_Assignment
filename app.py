1b5d6f183d47573d92d069a3cfc62a40c3c61944
ubaid <ubaid@gmail.com>
2026-06-17 14:17:30 +0530

    Added todo frontend page

--- app.py
+++ app.py
@@ -45,6 +45,10 @@ def submit():
 @app.route('/success')
 def success():
     return render_template('success.html')
+    
+@app.route('/todo')
+def todo():
+    return render_template('todo.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
